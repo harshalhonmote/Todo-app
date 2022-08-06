3208c0d993615f6bac15e46beeacc24e94d48ad0
Harshal Honmote <harshal09honmote@gmail.com>
2022-08-06 14:49:16 +0530

    Updates

---
+++ precache-manifest.6c7627aad87f88460f3cab961f2fd56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec375e9b7cc68fc3d33",
+    "url": "/React-project/static/css/main.dc6b0af2.chunk.css"
+  },
+  {
+    "revision": "2ec375e9b7cc68fc3d33",
+    "url": "/React-project/static/js/main.2ec375e9.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/React-project/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "7372631245bdf377fbdf",
+    "url": "/React-project/static/js/2.73726312.chunk.js"
+  },
+  {
+    "revision": "561a2648178b83b17c96",
+    "url": "/React-project/static/js/runtime~main.561a2648.js"
+  },
+  {
+    "revision": "976f48b979703a411795c05abc5cf545",
+    "url": "/React-project/index.html"
+  }
+];
